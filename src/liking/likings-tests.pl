0f498a17c6f5a8eae43126c2078b646472ac9706
Marek Dudek <marek.dudek@gmail.com>
2015-05-17 14:07:05 +0200

    Tested term inequality and variable bindings

--- src/liking/likings-tests.pl
+++ src/liking/likings-tests.pl
@@ -2,13 +2,37 @@
 :- use_module(likings).
 
 
-/* Simple fact. */
-test(wtf) :-
+/* Simple facts */
+
+test(fact_is_true) :-
 	likes(laura, flowers).
 
-/* Untrue condition. */
-test(wtf, [fail]) :-
+test(fact_is_not_true, [fail]) :-
 	likes(laura, thomas).
+
+
+/* Single variable bining */
+
+test(variable_bound_properly) :-
+	likes(laura, X),
+	X =@= flowers.
+
+test(variable_bound_properly_variant, [true( X =@= flowers)]) :-
+	likes(laura, X).
+
+
+/* Multiple variables binding */
+test(multiple_variables_bound_properly) :-
+	likes(X, Y),
+	X =@= laura,
+	Y =@= flowers.
+
+test(multiple_variables_bound_properly_variant, [true( X-Y =@= laura-flowers )]) :-
+	likes(X, Y).
+
+/* Multiple bindings */
+test(multiple_variables_non_deterministic, all(X-Y =@= [laura-flowers, thomas-laura])) :-
+	likes(X, Y).
 
 
 :- end_tests(likings).
