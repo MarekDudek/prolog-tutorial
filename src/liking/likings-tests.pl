50dd13fce5020fa0ecda466ae82a6afba283bd5f
Marek Dudek <marek.dudek@gmail.com>
2015-05-17 16:13:42 +0200

    Ignoring tests, setup and teardown

--- src/liking/likings-tests.pl
+++ src/liking/likings-tests.pl
@@ -7,7 +7,7 @@
 test(fact_is_true) :-
 	likes(laura, flowers).
 
-test(fact_is_not_true, [fail]) :-
+test(fact_is_not_true, fail) :-
 	likes(laura, thomas).
 
 
@@ -17,7 +17,7 @@ test(variable_bound_properly) :-
 	likes(laura, X),
 	X =@= flowers.
 
-test(variable_bound_properly_variant, [true( X =@= flowers)]) :-
+test(variable_bound_properly_variant, true(X =@= flowers)) :-
 	likes(laura, X).
 
 
@@ -27,12 +27,34 @@ test(multiple_variables_bound_properly) :-
 	X =@= laura,
 	Y =@= flowers.
 
-test(multiple_variables_bound_properly_variant, [true( X-Y =@= laura-flowers )]) :-
+test(multiple_variables_bound_properly_variant, true(X-Y =@= laura-flowers)) :-
 	likes(X, Y).
 
 /* Multiple bindings */
 test(multiple_variables_non_deterministic, all(X-Y =@= [laura-flowers, thomas-laura])) :-
 	likes(X, Y).
 
+
+/* Blocked test */
+
+test(blocked_intentionally, blocked(just_to_check_blocking)) :-
+	fail.
+
+test(intentionally_broken, fixme(just_to_check_fixme)) :-
+	fail.
+
+/* Conditional tests */
+
+test(with_failing_condition, condition(fail)) :-
+	format('!!! this should be executed !!!~n'),
+	fail.
+
+test(with_fulfilled_condition, condition(true)) :-
+	true.
+
+/* Test with setup and teardown */
+
+test(test_with_pre_and_post_actions, [setup(true), cleanup(true)]) :-
+	true.
 
 :- end_tests(likings).
