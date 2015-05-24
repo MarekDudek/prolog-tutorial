3279f6a6b98246d43f8bb2a5b6359029a68a5d6c
Marek Dudek <marek.dudek@gmail.com>
2015-05-24 17:56:58 +0200

    CI scripts reworked

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-
 THIS_FILE=$(readlink -f $0)
 THIS_DIR=`dirname ${THIS_FILE}`
 
 rm -f ./out/standalone
 ${THIS_DIR}/../src/application/build-standalone.sh
 
-
-${THIS_DIR}/../src/myapp/load_test.sh
-${THIS_DIR}/../src/liking/load_test.sh
+${THIS_DIR}/load_test.sh
--- bin/load_test.sh
+++ bin/load_test.sh
@@ -3,5 +3,7 @@
 THIS_FILE=$(readlink -f $0)
 THIS_DIR=`dirname ${THIS_FILE}`
 
-${THIS_DIR}/../src/myapp/load_test.sh
-${THIS_DIR}/../src/liking/load_test.sh
+SOURCE_DIR=${THIS_DIR}/../src
+
+${SOURCE_DIR}/myapp/load_test.sh
+${SOURCE_DIR}/liking/load_test.sh
