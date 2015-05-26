3b64227e4f445fe40430d2e368883879007277ff
Marek Dudek <marek.dudek@gmail.com>
2015-05-26 23:12:48 +0200

    Excercise 1.3 cont.

--- src/basics/family.pl
+++ src/basics/family.pl
@@ -2,13 +2,28 @@
 
 /* Already defined */
 
+father(wiktor, dionizy).
+
+father(dionizy, rafal).
 father(krzysztof, marek).
+
+father(rafal, rafals_daughter).
+
+mother(daniela, krzysztof).
+
+mother(teresa, rafal).
 mother(lucja, marek).
 
+male(wiktor).
+male(dionizy).
 male(krzysztof).
+male(rafal).
 male(marek).
 
+female(daniela).
+female(teresa).
 female(lucja).
+
 
 parent(X, Y) :-
 	father(X, Y) ; mother(X, Y).
@@ -20,6 +35,13 @@ diff(X, Y) :-
 
 is_mother(X) :-
 	mother(X, _).
+	
+is_father(X) :-
+	father(X, _).
+
+is_son(X) :-
+	(mother(_, X) ; father(_, X)),
+	male(X).
 
 /* Tests */
 
@@ -38,13 +60,33 @@ test('differences 1') :-
 test('differences 2') :-
 	diff(lucja, marek).
 	
-test('mother') :-
-	is_mother(lucja).
+test('mother', nondet) :-
+	is_mother(daniela),
+	is_mother(lucja),
+	is_mother(teresa).
 	
 test('mother', fail) :-
 	is_mother(krzysztof),
 	is_mother(marek).
+	
+test('father', nondet) :-
+	is_father(wiktor),
+	is_father(dionizy),
+	is_father(krzysztof).
+	
+test('father', fail) :-
+	is_father(lucja),
+	is_father(marek).
+	
+test('son', nondet) :-
+	is_son(dionizy),
+	is_son(krzysztof),
+	is_son(rafal),
+	is_son(marek).
 
+test('son', fail) :-
+	is_son(rafals_daughter).
+	
 :- end_tests('family').
 
 
