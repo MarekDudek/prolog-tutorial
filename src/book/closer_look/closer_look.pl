2cd626d57b1b8d43c5cf10f5658c66ebd1eb9c5e
Marek Dudek <marek.dudek@gmail.com>
2015-05-27 23:08:38 +0200

    Arithmetic started

--- src/book/closer_look/closer_look.pl
+++ src/book/closer_look/closer_look.pl
@@ -10,6 +10,10 @@ owns(mary, moby_dick).
 
 owns(john, book(wuthering_heights, author(emily, bronte))).
 
+/* 
+ * Test suite 
+ */
+
 :- begin_tests('closer look').
 
 test('owning', nondet) :-
@@ -19,5 +23,12 @@ test('matching variables in structure') :-
 	owns(john, book(X, author(Y, bronte))),
 	assertion(X == wuthering_heights),
 	assertion(Y == emily).
+	
+test('operators do not carry out math', fail) :-
+	3 + 4 == 7.
+	
+test('carry out math') :-
+	7 is 3 + 4.
+
 
 :- end_tests('closer look').
