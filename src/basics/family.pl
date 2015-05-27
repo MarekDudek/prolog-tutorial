08355ba7fa6cc206bf2eb1e7a8747f7815aa01e4
Marek Dudek <marek.dudek@gmail.com>
2015-05-27 21:03:24 +0200

    Grandpa rule implemented

--- src/basics/family.pl
+++ src/basics/family.pl
@@ -31,7 +31,7 @@ female(lucja).
 
 parent(X, Y) :-
 	father(X, Y) ; mother(X, Y).
-	
+
 diff(X, Y) :-
 	X \= Y.
 
@@ -39,14 +39,14 @@ diff(X, Y) :-
 
 is_mother(X) :-
 	mother(X, _).
-	
+
 is_father(X) :-
 	father(X, _).
 
 is_son(X) :-
 	(mother(_, X) ; father(_, X)),
 	male(X).
-	
+
 sister_of(X, Y) :-
 	female(X),
 	mother(M, X),
@@ -54,6 +54,11 @@ sister_of(X, Y) :-
 	mother(M, Y),
 	father(F, Y),
 	diff(X, Y).
+
+grandpa_of(Grandpa, Grandson) :-
+	male(Grandpa),
+	(father(Grandpa, Father) , father(Father, Grandson));
+	(father(Grandpa, Mother) , mother(Mother, Grandson)).
 
 /* Tests */
 
@@ -64,35 +69,35 @@ test('my father', nondet) :-
 
 test('my mother') :-
 	parent(lucja, marek).
-	
+
 test('differences 1') :-
 	diff(krzysztof, marek).
-	
+
 test('differences 2') :-
 	diff(lucja, marek).
-	
+
 test('mother', nondet) :-
 	is_mother(daniela),
 	is_mother(lucja),
 	is_mother(teresa).
-	
+
 test('mother', fail) :-
 	is_mother(krzysztof).
 
 test('mother', fail) :-
 	is_mother(marek).
-	
+
 test('father', nondet) :-
 	is_father(wiktor),
 	is_father(dionizy),
 	is_father(krzysztof).
-	
+
 test('father', fail) :-
 	is_father(lucja).
 
 test('father', fail) :-
 	is_father(marek).	
-	
+
 test('son', nondet) :-
 	is_son(dionizy),
 	is_son(krzysztof),
@@ -101,16 +106,24 @@ test('son', nondet) :-
 
 test('son', fail) :-
 	is_son(rafals_daughter).
-	
+
 test('sister') :-
 	sister_of(lucja, karol),
 	sister_of(lucja, henryk).
 	
 test('sister', fail) :-
 	sister_of(karol, lucja).
-	
+
 test('sister', fail) :-
 	sister_of(lucja, lucja).
+
+test('grandpa', nondet) :-
+	grandpa_of(wiktor, rafal),
+	grandpa_of(antoni, marek).
+
+test('grandpa', fail) :-
+	grandpa_of(wiktor, marek).
+
 	
 :- end_tests('family').
 
