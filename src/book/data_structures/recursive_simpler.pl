6f0c7b87ff372b2e66a27f1d60d2d789f5bf135b
Marek Dudek <marek.dudek@gmail.com>
2015-05-30 15:19:59 +0200

    Solution similar to one for Excercise 3.1

--- src/book/data_structures/recursive_simpler.pl
+++ src/book/data_structures/recursive_simpler.pl
@@ -3,17 +3,28 @@
 
 list(a, [1, 2, 3, 4]).
 list(b, [3, 4, 5, 6]).
-list(c, [2, 3, 6, 7]).
+list(c, [2, 3, 6, 8]).
 
 greater([], []).
 greater([H1|T1], [H2|T2]) :-
 	H1 > H2,
 	greater(T1, T2).
 
+/* At least one element greater than coresponding */
 sometimes_greater([H1|_T1], [H2|_T2]) :-
 	H1 > H2.
 sometimes_greater([_H1|T1], [_H2|T2]) :-
 	sometimes_greater(T1, T2).
+
+much_greater(N1, N2) :-
+	Threshold is N2 + 2,
+	N1 >= Threshold.
+	
+/* At least one element greater by at least 2 */
+sometimes_much_greater([H1|_T1], [H2|_T2]) :-
+	much_greater(H1, H2).
+sometimes_much_greater([_H1|T1], [_H2|T2]) :-
+	sometimes_much_greater(T1, T2).
 
 :- begin_tests('recursive comparisons').
 
@@ -56,5 +67,21 @@ test('not a ># b', [fail]) :-
 	list(a, A),
 	list(b, B),
 	sometimes_greater(A, B).
-	
+
+test('3 much greater then 1') :-
+	much_greater(3, 1).
+
+test('2 not much greater then 1', fail) :-
+	much_greater(2, 1).
+
+test('c >># b', nondet) :-
+	list(b, B),
+	list(c, C),
+	sometimes_much_greater(C, B).
+
+test('not b >># c', fail) :-
+	list(b, B),
+	list(c, C),
+	sometimes_much_greater(B, C).
+			
 :- end_tests('recursive comparisons').
