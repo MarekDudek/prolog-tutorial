dce3704b883efec52dbcb6632a57efc68c97d159
Marek Dudek <marek.dudek@gmail.com>
2015-05-30 18:27:03 +0200

    Assembling bike with accumulators

--- src/book/data_structures/joining_structures.pl
+++ src/book/data_structures/joining_structures.pl
@@ -18,6 +18,7 @@ assembly(hub, [gears, axle]).
 assembly(axle, [bolt, nut]).
 
 
+
 parts_of(BasicPart, [BasicPart]) :-
 	basicpart(BasicPart).
 parts_of(Assembly, BasicParts) :-
@@ -30,6 +31,21 @@ parts_of_list([H|T], BasicParts) :-
 	parts_of_list(T, TailParts),
 	append(HeadParts, TailParts, BasicParts).
 
+%% The same with accumulators
+
+parts_of_2(Composite, BasicParts) :-
+	parts_of_2(Composite, [], BasicParts).
+
+parts_of_2(BasicPart, Acc, [BasicPart|Acc]) :-
+	basicpart(BasicPart).
+parts_of_2(Assembly, Acc, BasicParts) :-
+	assembly(Assembly, SubParts),
+	parts_of_list_2(SubParts, Acc, BasicParts).
+	
+parts_of_list_2([], Acc, Acc).
+parts_of_list_2([H|T], Acc, BasicParts) :-
+	parts_of_2(H, Acc, HeadParts),
+	parts_of_list_2(T, HeadParts, BasicParts).
 
 :- begin_tests('joining structures').
 
@@ -43,5 +59,16 @@ test('parts of bike', nondet) :-
 		[spoke, rim, gears, bolt, nut, 
 		 spoke, rim, gears, bolt, nut,
 		 rearframe, fork, handles]).
+
+test('acc parts of wheel', nondet) :- 
+	parts_of_2(wheel, Parts),
+	assertion(Parts == [nut,bolt,gears,rim,spoke]).
+
+test('acc parts of bike', nondet) :- 
+	parts_of_2(bike, Parts),
+	assertion(Parts == 
+		[handles,fork,rearframe,
+		 nut,bolt,gears,rim,spoke,
+		 nut,bolt,gears,rim,spoke]).
 
 :- end_tests('joining structures').
