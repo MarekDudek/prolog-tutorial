56d173a67c3b8748afb837cdd885eefa202a655f
Marek Dudek <marek.dudek@gmail.com>
2015-05-31 12:15:49 +0200

    K-th element of the list

--- src/ninety_nine/lists/kth_element.pl
+++ src/ninety_nine/lists/kth_element.pl
@@ -1,12 +1,27 @@
 :- module(kth_element,[		
 ]).
 
+%% element_at(?Elem, ?List, ?K).
+%
+%  True if Elem occurs at position K in List. Count starts at 1.
+
+element_at(H, [H|_T], 1). 
+element_at(Elem, [_H|T], K) :-
+	K1 is K - 1,
+	element_at(Elem, T, K1).
+
 
 /* 
  * Test suite 
  */
 :- begin_tests('k-th element').
 
-test('') :- true.
+test('1-st element of the list', nondet) :- 
+	element_at(E, [a, b, c], 1),
+	assertion(E == a).
 
+test('3-rd element of the list', nondet) :- 
+	element_at(E, [a, b, c, d, e], 3),
+	assertion(E == c).
+	
 :- end_tests('k-th element').
