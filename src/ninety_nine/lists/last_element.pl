fe0d3c2af274a02744cdff40c7b46fca3b180d05
Marek Dudek <marek.dudek@gmail.com>
2015-05-31 11:32:02 +0200

    Last element - simplest solution

--- src/ninety_nine/lists/last_element.pl
+++ src/ninety_nine/lists/last_element.pl
@@ -2,8 +2,25 @@
 ]).
 
 
+%% last_element(?Elem, ?List).
+%
+%  True if Elem is the last element of List.
+
+last_element(Elem, [Elem]).
+last_element(Elem, [_H|T]) :-
+	last_element(Elem, T).
+
+/* 
+ * Test suite 
+ */
 :- begin_tests('last element').
 
-test('') :- true.
+test('Last element of one-element list', nondet) :- 
+  	last_element(E, [e]),
+  	assertion(E == e).
+
+test('Last element of multi-element list', nondet) :- 
+  	last_element(E, [a, b, c, d, e]),
+  	assertion(E == e).
 
 :- end_tests('last element').
