0d049436b042b0985c00f851d1bf45891f72c364
Marek Dudek <marek.dudek@gmail.com>
2015-06-02 22:45:47 +0200

    Auxiliary tests for packing list

--- src/ninety_nine/lists/pack_cons_dups.pl
+++ src/ninety_nine/lists/pack_cons_dups.pl
@@ -8,8 +8,8 @@ pack_cons_dups([], []).
 pack_cons_dups([A], [[A]]).
 pack_cons_dups([A, A], [[A, A]]).
 pack_cons_dups([A, B], [[A], [B]]).
-%pack_cons_dups([A, A, A], [[A, A, A]]).
-%pack_cons_dups([H 
+
+
 
 /* 
  * Test suite 
@@ -32,9 +32,18 @@ test('Two different elements list', nondet) :-
 	pack_cons_dups([a, b], L),
 	assertion(L == [[a], [b]]).
 
-%test('Three equal elements list', nondet) :-
-%	pack_cons_dups([a, a, a], L),
-%	assertion(L == [[a, a, a]]).
+
+
+test('matching head') :-
+	L = [elem],
+	L = [H | T],
+	H = elem,
+	T = [].
 	
-	
+test('match embedded head') :-
+	L = [[a], [b]],
+	L = [[A] | T],
+	A = a,
+	T = [[b]].
+		
 :- end_tests('pack consecutive duplicates').
